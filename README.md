bddd1866249dfed2ff2ae0682a99a3c2992ad79a
Phil Tsaryk <phil@tsaryk.com>
2018-12-08 01:26:58 +0100

    Add initial functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # @phts/tslint-config
+
+My config for TSLint. It is based on [my ESLint config](https://github.com/phts/eslint-config)
+with additional TypeScript-specific rules.
+
+Import of ESLint config into TSLint is provided by
+[tslint-eslint-rules](https://github.com/buzinas/tslint-eslint-rules) and
+[tslint-import-eslint-config](https://github.com/teppeis/tslint-import-eslint-config)
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const ESLINT_CONFIG = {
+  extends: '@phts',
+}
+
+const importEslintConfig = require('tslint-import-eslint-config')
+const tslintConfig = importEslintConfig(ESLINT_CONFIG)
+
+tslintConfig.extends.unshift('tslint:recommended')
+Object.assign(tslintConfig.rules, {
+  // fix tslint-import-eslint-config
+  'array-bracket-spacing': [true, 'never'],
+  'brace-style': [true, '1tbs'],
+  'no-consecutive-blank-lines': [true, 2],
+  'object-literal-key-quotes': [true, 'as-needed'],
+  quotemark: [true, 'single', 'jsx-double', 'avoid-escape'],
+  semicolon: [true, 'never'],
+  'trailing-comma': [true, {
+    multiline: 'always',
+    singleline: 'never',
+  }],
+  'variable-name': [true, 'check-format', 'allow-pascal-case'],
+
+  // fix tslint:recommended
+  'arrow-parens': [true, 'ban-single-arg-parens'],
+  'interface-name': false,
+  'interface-over-type-literal': false,
+  'member-access': [true, 'no-public'],
+  'no-var-requires': false,
+  'object-literal-sort-keys': false,
+  'ordered-imports': false,
+
+  // extra rules
+  'no-any': true,
+  'no-inferrable-types': true,
+})
+
+module.exports = tslintConfig
